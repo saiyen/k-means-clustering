028a09451aaaf769a6d0b92c3eb57f60150df0df
Saiyen <chilli_trunks@hotmail.com>
2016-05-19 17:15:59 +0200

    v1 Read data and set in array[ clients][wines]
    Read the data of prefered wines and set them in an 2-d array of clients
    and wines

---
+++ k-means clustering/FileReader/ReadFromFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace item_user.FileReader
+{
+    class ReadFromFile
+    {
+        static private string line;
+        static string project_path = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+        static StreamReader file;
+
+        static int[,] PreferedWinesOfClients = new int[100, 32];
+
+        public static int[,] getPreferedWinesOfClients()
+        {
+            // Read the file and display it line by line.
+            file = new StreamReader(project_path + "/DataSet/WineData.csv");
+            int count = 0;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                var temp = line.Split(',');
+
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    //Console.WriteLine(temp[i]);
+                    //toevoegen in de array boven
+                    PreferedWinesOfClients[i, count] = int.Parse(temp[i]);
+                }
+
+                count++;
+            }
+            file.Close();
+
+            return PreferedWinesOfClients;
+        }
+    }
+}
---
+++ k-means clustering/Program.cs
@@ -0,0 +1,21 @@
+﻿using item_user.FileReader;
+using System;
+
+namespace k_means_clustering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] PreferedWinesOfClients = ReadFromFile.getPreferedWinesOfClients();
+
+            for (int i = 0; i < 100; i++)
+            {
+                for(int j = 0; j < 32; j++)
+                {
+                    Console.WriteLine(PreferedWinesOfClients[i, j]);
+                }
+            }
+        }
+    }
+}
